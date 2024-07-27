794955519ac06b791a44fe2f61d054ca05be2fa6
andreu <andreu.country.dev@gmail.com>
2024-07-27 21:43:53 +0200

    new files

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Formulario enviado');
+document.addEventListener('DOMContentLoaded', () => {
+    const esTab = document.getElementById('es-tab');
+    const enTab = document.getElementById('en-tab');
+
+    esTab.addEventListener('click', (e) => {
+        e.preventDefault();
+        switchLanguage('es');
+    });
+
+    enTab.addEventListener('click', (e) => {
+        e.preventDefault();
+        switchLanguage('en');
+    });
+
+    function switchLanguage(lang) {
+        const elements = document.querySelectorAll('[data-lang-es]');
+
+        elements.forEach(el => {
+            const text = el.getAttribute(`data-lang-${lang}`);
+            if (text) {
+                el.textContent = text;
+            }
+        });
+    }
 });
